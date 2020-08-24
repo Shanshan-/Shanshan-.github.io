5f5e5393adddc7ba20799d379380e2c71a360b53
Shanshan- <shanon5656@gmail.com>
2020-08-24 00:13:38 -0400

    finished automating filling from file of projects table

--- javascripts/projectfiller.js
+++ javascripts/projectfiller.js
@@ -7,13 +7,48 @@ function fillProjects() {
     tableHTML += "<button class=\"button project-button\"></button></div></td>";
 
     //setup variables and console outputs
-    var table, curCell, curRow, titleRow, descRow, tmp;
-    table = document.getElementById("project-table1");
+    var table, curRow, data, tmp, clickTxt;
+    table = document.getElementById("project-table");
     console.log(table);
 
-    //fill in a row
-    curRow = table.insertRow();
-    curRow.setAttribute("class", "project");
-    curRow.innerHTML = tableHTML;
-    tmp = curRow.getElementsByClassName("project-img");
+    //get project data
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            //data.innerHTML = this.responseText;
+            data = JSON.parse(this.responseText);
+            console.log(data);
+
+            //start building the row
+            data.projects.forEach(function(proj){
+                curRow = table.insertRow();
+                curRow.setAttribute("class", "project");
+                curRow.setAttribute("id", proj.projectid);
+                curRow.innerHTML = tableHTML;
+
+                //fill in information
+                tmp = curRow.getElementsByClassName("project-img");
+                tmp[0].setAttribute("src", proj.imgsrc);
+                tmp = curRow.getElementsByClassName("project-name");
+                tmp[0].innerText = proj.name;
+                tmp = curRow.getElementsByClassName("project-lang");
+                tmp[0].innerText = "Written in " + proj.language;
+                tmp = curRow.getElementsByClassName("project-desc");
+                tmp[0].innerText = proj.description;
+                tmp = curRow.getElementsByClassName("project-button");
+
+                //set reference button accordingly
+                if(proj.github == "") {
+                    tmp[0].innerText = "Private";
+                    tmp[0].disabled = true;
+                } else {
+                    tmp[0].innerText = "GitHub Repo";
+                    clickTxt = "window.open(\'" + proj.github + "\', \'_blank\')";
+                    tmp[0].setAttribute("onclick", clickTxt);
+                }
+            });
+        }
+    };
+    xmlhttp.open("GET", "projects.json", true);
+    xmlhttp.send();
 }
