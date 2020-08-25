42aff278b97f265c043a51e90ee46845bbfed6d2
Shanshan- <shanon5656@gmail.com>
2020-08-25 00:28:53 -0400

    cleaned up file

--- javascripts/topbutton.js
+++ javascripts/topbutton.js
@@ -1,39 +1,7 @@
-/*
-//get the button
-//NOTE: this will only add functionality to the FIRST top button on page
-var btn = document.getElementsByClassName("top-button")[0];
-var threshold = 20; //scrolling threshold
-
-//hide the button unless a certain distance down the page
-window.onscroll = scrollFunction();
-function scrollFunction() {
-    if (document.body.scrollTop() > threshold || document.documentElement.scrollTop() > threshold) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-}
-*/
-/*$(window).scroll(function() {
-    if($(window).scrollTop() > threshold) {
-        $('.top-button')[0].fadeIn();
-    } else {
-        $('.top-button')[0].fadeOut();
-    }
-});*//*
-
-
-//add "to top" functionality
-function topFunction() {
-    try {
-        window.scrollTo({top:0, behavior: "smooth"});
-    } catch {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-    }
-}*/
 //functionaity: https://html-online.com/articles/dynamic-scroll-back-top-page-button-javascript/
 //styling: https://codepen.io/matthewcain/pen/ZepbeR
+//when using this, make sure to include the following in the html page:
+//  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 $(window).scroll(function() {
     var height = $(window).scrollTop();
     if (height > 100) {
